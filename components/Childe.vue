02aebade3b6bedba663bf6f3e2221eb2cfb1def9
MinamideTakaaki <minamide.takataka@gmail.com>
2022-08-11 22:56:45 +0900

    cannnot read property '0' of undefined

---
+++ components/Childe.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="child">
+		{{ user.name[0] }} <br>
+		{{ user.name[1] }} <br>
+		{{ user.age }}
+	</div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+	props: ['user']
+})
+</script>
---
+++ pages/home.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="parent">
+		<Childe :user="user" />
+	</div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import Childe from '~/components/Childe.vue';
+
+export default {
+	components: {
+		Childe
+	},
+	data() {
+		return {
+			user: [
+				{
+					"name": [],
+					"age": ""
+				}
+			]
+		}
+	},
+	created: function () {
+		axios.get('list_data.json')
+			.then(response => {
+				let json = response.data;
+				this.user = json;
+			})
+			.catch(err =>{
+				console.error(err)
+			})
+	}
+}
+
+</script>
